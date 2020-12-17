6ab24653d9aa0c289499f408167a4cc22d7e54c9
Tega Oke <oketegah@gmail.com>
2020-12-17 13:51:50 +0100

    create auth route

---
+++ src/routes/auth.ts
@@ -0,0 +1,19 @@
+import { Router } from 'express';
+import { celebrate as validate } from 'celebrate';
+import { generateUserToken } from '../controllers/auth';
+import authValidation from '../validations/auth';
+const router = Router();
+
+router
+  .route('/')
+  .post(
+    [
+      validate(authValidation.generateToken, {
+        abortEarly: false,
+        stripUnknown: true,
+      }),
+    ],
+    generateUserToken,
+  );
+
+export default router;

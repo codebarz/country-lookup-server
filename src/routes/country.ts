c32cce8aa1a5eb3de8f36979c32a736db257b012
whitehox <oketegah@gmail.com>
2020-12-19 15:24:48 +0100

    add rate limiting middleware to coutry route

--- src/routes/country.ts
+++ src/routes/country.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import { getCountry } from '../controllers/country';
 import checkToken from '../middlewares/auth';
+import limitRequestRate from '../middlewares/rateLimiting';
 
 const router = Router();
 
-router.route('/:country').post(checkToken, getCountry);
+router.route('/:country').post(checkToken, limitRequestRate, getCountry);
 
 export default router;
